60b656a309bf9f63e70d63c4603163e5d84efec0
RinkakuGomez <37704416+RinkakuGomez@users.noreply.github.com>
2020-07-03 12:21:39 +0200

    Update Training
    Visible = 4
    Hidden = 2, 4, 8

--- MultiTrain.py
+++ MultiTrain.py
@@ -11,20 +11,23 @@ import pruebasAnalisisEst
     
 if __name__== '__main__':
     
-    arr_BatchSize = [32, 16, 8]
-    arr_LR = [0.01, 1.0, 0.1]
-    arr_hidden = [8, 6] 
-    num_epoch = 5000
-    n_visible = 3
+    arr_BatchSize = [64, 40, 32, 16, 8]
+    arr_LR = [0.01, 1.0, 0.1, 0.001]
+    arr_hidden = [2, 8, 4] 
+    num_epoch = 10000
+    n_visible = 4
     k = 1
-    arr_fString = ['Sex','Age','Survived']
+    arr_fString = ['Sex','Age','Survived', 'PClass']
     i = 1 
+    
+    #prueba = pruebasAnalisisEst.PruebasEstadistica(n_epoch=1, lr=1.0, bs=32, num_visible=4, num_hidden=2, num_k=1, arr_fieldString=arr_fString)
+    #prueba.prueba_ClassInput()
     for n_hidden in arr_hidden:
         for learning_rate in arr_LR:
             for batch_size in arr_BatchSize:
                 
                 print('Entrenamiento número: '+str(i))
-                name_CSVPrueba = 'pruebasPrecision/pruebaMultiH'+str(n_hidden)+'LR'+str(learning_rate)+'BS'+str(batch_size)+'.txt'
+                name_CSVPrueba = 'pruebasPrecision/pruebaMultiV'+str(n_visible)+'H'+str(n_hidden)+'E'+str(num_epoch)+'LR'+str(learning_rate)+'BS'+str(batch_size)+'.txt'
                 prueba = pruebasAnalisisEst.PruebasEstadistica(n_epoch=num_epoch, lr=learning_rate, bs=batch_size, num_visible=n_visible, num_hidden=n_hidden, num_k=k, arr_fieldString=arr_fString)
                 prueba.pruebaTest(name_CSVPrueba)
                 i+=1
