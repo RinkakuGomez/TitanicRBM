80a8d956dab19988b5fd4670b2385c2fbde990c4
RinkakuGomez <javier.gomez.ines@gmail.com>
2021-03-11 16:13:58 +0000

    Subida a nueva rama pruebas modelo

---
+++ auxMultiTrain2200.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 10:34:18 2020
+
+@author: Pc
+"""
+
+import pruebasAnalisisEst
+import os
+    
+if __name__== '__main__':
+    
+    arr_BatchSize = [8]
+    arr_LR = [0.001]
+    arr_hidden = [4] 
+    num_epoch = 2000
+    n_visible = 4
+    k = 1
+    arr_fString = ['Sex','Age','Survived', 'PClass']
+    i = 1 
+    name_dataSet = 'Titanic2200.csv'
+    dir_name = 'pruebasPrecision2200'
+    
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+    for n_hidden in arr_hidden:
+        for learning_rate in arr_LR:
+            for batch_size in arr_BatchSize:
+                
+                print('Entrenamiento número: '+str(i))
+                name_CSVPrueba = dir_name+'/pruebaMultiV'+str(n_visible)+'H'+str(n_hidden)+'E'+str(num_epoch)+'LR'+str(learning_rate)+'BS'+str(batch_size)+'.txt'
+                prueba = pruebasAnalisisEst.PruebasEstadistica(nameDataset = name_dataSet,n_epoch=num_epoch, lr=learning_rate, bs=batch_size, num_visible=n_visible, num_hidden=n_hidden, num_k=k, arr_fieldString=arr_fString)
+                prueba.pruebaTest(name_CSVPrueba)
+                i+=1
